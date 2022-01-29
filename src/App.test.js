ec3f93c82e4020653551e600c4bd374ae4505979
Wojciech Bylica <wojtekbyl@gmail.com>
2022-01-29 01:11:13 +0100

    basic test set

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders in header myMetronome', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/myMetronome/i);
+  expect(headerText).toBeInTheDocument();
 });
